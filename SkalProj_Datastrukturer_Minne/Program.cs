f947e7b44cd04972f8e5ed1dca2d6e01f4466d91
JKL <jarmogbg1975@outlook.com>
2024-03-18 19:40:53 +0100

    Klar

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -68,17 +68,49 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch statement with cases '+' and '-'
              * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
              * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * '0': Exit to main menu
              * In both cases, look at the count and capacity of the list
              * As a default case, tell them to use only + or -
              * Below you can see some inspirational code to begin working.
             */
+            //1.
+            //2. Listans kapacitet ökar när count == capacity och man försöker lägga till ett element.
+            //3. Capacity ökar med 4
+            //4. För att det skulle bli för mycket overhead att skapa en ny array varje gång man lägger till ett nytt element.
+            //5. Nej, listans capacity förblir densamma.
+            //6. När man vet antalet element som den ska innehålla.
+            List<string> theList = new List<string>();
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            bool continueLoop = true;
 
-            //switch(nav){...}
+            while (continueLoop)
+            {
+                Console.WriteLine("\nPlease add or remove from list by using +(string) or -(string), 0 to exit\n");
+                
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1);
+
+                switch (nav)
+                {
+                    case '+':
+                        Console.WriteLine($"\nBefore adding element. List count: {theList.Count}, List capacity: {theList.Capacity}");
+                        theList.Add(value);
+                        Console.WriteLine($"\nAfter adding element. List count: {theList.Count}, List capacity: {theList.Capacity}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"\nBefore removing element. List count: {theList.Count}, List capacity: {theList.Capacity}");
+                        theList.Remove(value);
+                        Console.WriteLine($"\nAfter removing element. List count: {theList.Count}, List capacity: {theList.Capacity}");
+                        break;
+                    case '0':
+                        continueLoop = false;
+                        break;                        
+                    default:
+                        Console.WriteLine("Please enter valid input (+(string), -(string) or 0 to exit.");
+                        break;
+                }
+            }
         }
 
         /// <summary>
@@ -91,11 +123,52 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+
+            Queue<string> theQueue = new Queue<string>();
+
+            bool continueLoop = true;
+
+            while (continueLoop)
+            {
+                Console.WriteLine("\nPlease add or remove from queue by using +(string) or -, 0 to exit\n");
+
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = "";
+                if (input.Count() > 1)
+                {
+                    value = input.Substring(1);
+                }
+
+                switch (nav)
+                {
+                    case '+':
+                        Console.WriteLine($"\nBefore adding element. Queue count: {theQueue.Count}");
+                        theQueue.Enqueue(value);
+                        Console.WriteLine($"\nAfter adding element. Queue count: {theQueue.Count}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"\nBefore removing element. Queue count: {theQueue.Count}");
+                        if (theQueue.Count > 0)
+                        {
+                            string removedItem = theQueue.Dequeue();
+                            Console.WriteLine($"\nAfter removing element. Queue count: {theQueue.Count}, removed from Queue : {removedItem}");
+                        }
+                        break;
+                    case '0':
+                        continueLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter valid input (+(string), -(string) or 0 to exit.");
+                        break;
+                }
+            }
         }
 
         /// <summary>
         /// Examines the datastructure Stack
         /// </summary>
+        /// 1. För att den som kommer först till kön får vänta längst, så länge det kommer in nya köare så får dom som redan står i kön fortsätta köa.
         static void ExamineStack()
         {
             /*
@@ -103,8 +176,30 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+            Stack<char> stack = new Stack<char>();
+
+            Console.WriteLine("\nPlease enter string to reverse:\n");
+
+            string input = Console.ReadLine();
+            
+            foreach(char c in input)
+            {
+                stack.Push(c);
+            }
+
+            string output = "";
+            int stackCount = stack.Count;
+
+            for(int i = 0; i < stackCount; i++)
+            {
+                output = output + stack.Pop();
+            }
+
+            Console.WriteLine($"\nReversed string : {output}\n");
         }
 
+        //1. Stacken. För varje vänsterparentes pusha parentesen på stacken. För varje högerparentes, poppa från stacken & se så du får motsvarande vänsterparentes. Ifall inte, så är strängen fel
+        //   När input är slut, se så att stacken är tom, ifall element kvar, är strängen fel.
         static void CheckParanthesis()
         {
             /*
@@ -112,7 +207,59 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
+            Stack<char> stack = new Stack<char>();
 
+            Console.WriteLine("\nPlease enter string to check for parenthesis:\n");
+
+            string input = Console.ReadLine();
+            bool incorrect = false;
+
+            foreach (char c in input)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                if(c == ')')
+                {
+                    var popped = stack.Pop();
+                    if (popped != '(')
+                    {
+                        incorrect = true;
+                        break;                        
+                    }
+                }
+                if (c == ']')
+                {
+                    var popped = stack.Pop();
+                    if (popped != '[')
+                    {
+                        incorrect = true;
+                        break;
+                    }
+                }
+                if (c == '}')
+                {
+                    var popped = stack.Pop();
+                    if (popped != '{')
+                    {
+                        incorrect = true;
+                        break;
+                    }
+                }
+            }
+            if(stack.Count != 0)
+            {
+                incorrect = true;
+            }
+
+            if (incorrect) {
+                Console.WriteLine("\nIncorrect parenthesis in input string\n");
+            }
+            else
+            {
+                Console.WriteLine("\nWelformed string.\n");
+            }
         }
 
     }
